e2f599e065108d7a1580748c86d2c644ceee8250
Adam Navarro <adamjnav@gmail.com>
2025-02-24 11:37:19 -0600

    remove infra project and logger refs

--- CogLog.App/Contracts/Logging/IAppLogger.cs
+++
@@ -1,8 +0,0 @@
-namespace CogLog.App.Contracts.Logging;
-
-public interface IAppLogger<T>
-{
-    void LogInfo(string message, params object[] args);
-
-    void LogWarning(string message, params object[] args);
-}
